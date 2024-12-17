e65e51deeddb2c776d6d94d8569e0ac2d3ecacf6
asteriskzuo <zuoyuhsywn@hotmail.com>
2024-12-17 14:09:01 +0800

    fix: turbo mode bug

--- ios/ChatRoom.h
+++ ios/ChatRoom.h
@@ -1,12 +1,7 @@
 
-#ifdef RCT_NEW_ARCH_ENABLED
-#import "RNChatRoomSpec.h"
 
-@interface ChatRoom : NSObject <NativeChatRoomSpec>
-#else
 #import <React/RCTBridgeModule.h>
 
 @interface ChatRoom : NSObject <RCTBridgeModule>
-#endif
 
 @end
